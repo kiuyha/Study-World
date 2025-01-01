e824edf1ed8dcdc52d41adc4835bcd97bc334468
Kiuyha <ketutshridhara@gmail.com>
2025-01-01 07:36:16 +0700

    add

--- website/static/js/user/template_user.js
+++ website/static/js/user/template_user.js
@@ -50,15 +50,17 @@ function fetch_notif(){
             }
             if (!notif[3]){
                 const svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#EAC452"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"/></svg>'
-                li.insertAdjacentHTML('afterbegin', svg);
-                li.id = `notif-${notif[0]}`;
                 if(notif[5]){
                     const a = document.createElement('a');
                     a.href = notif[5];
                     a.innerHTML = li.innerHTML;
+                    a.insertAdjacentHTML('afterbegin', '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/></svg>');
                     li.innerHTML = '';
                     li.appendChild(a);
+                }else{
+                    li.insertAdjacentHTML('afterbegin', svg);
                 }
+                li.id = `notif-${notif[0]}`;
                 aktivitas_list.appendChild(li);   
             } else{
                 const svg = '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" fill="#FFFFFF"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>'
